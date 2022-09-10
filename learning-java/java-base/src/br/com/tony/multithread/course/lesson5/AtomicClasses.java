c034eeda5d82fe89779f67e5a1719e77c0b8d901
tonyals <tony_luzsilva@hotmail.com>
2022-09-10 15:36:39 -0300

    feat: AtomicClasses examples

---
+++ learning-java/java-base/src/br/com/tony/multithread/course/lesson5/AtomicClasses.java
@@ -0,0 +1,36 @@
+package br.com.tony.multithread.course.lesson5;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+/*
+ * Atomic classes
+ * */
+public class AtomicClasses {
+
+    static AtomicInteger i = new AtomicInteger(-1);
+
+    /*
+    * AtomicReference permite usar um objeto como parâmetro e ter acesso a operações dele Thread-Safe
+    * sem uso de synchronized
+    * */
+    static AtomicReference<String> atomicReference = new AtomicReference<>("Reference");
+
+    public static void main(String[] args) throws InterruptedException {
+        MyRunnable runnable = new MyRunnable();
+        Thread t0 = new Thread(runnable);
+        Thread t1 = new Thread(runnable);
+        Thread t2 = new Thread(runnable);
+
+        t0.start();
+        t1.start();
+        t2.start();
+    }
+
+    public static class MyRunnable implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": " + i.incrementAndGet());
+        }
+    }
+}
