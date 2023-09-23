9ca441c829a3862b1d17a4ba3c36a6798d37569e
tony <tony_luzsilva@hotmail.com>
2023-09-23 16:06:48 -0300

    feat: add challenges

---
+++ algorithms-and-data-structure/src/br/com/tony/hackerrank/AppleAndOrange.java
@@ -0,0 +1,45 @@
+package br.com.tony.hackerrank;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true">Challenge link</a>
+ * */
+public class AppleAndOrange {
+    public static void main(String[] args) {
+        List<Integer> apples = Arrays.asList(-2, 2, 1);
+        List<Integer> oranges = Arrays.asList(5, -6);
+        int s = 7;  // starting point of sam's house location
+        int t = 11;  // ending point of sam's house location
+        int a = 5;  // location of apple tree
+        int b = 15;  // location of orange tree
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+    }
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s: integer, starting point of Sam's house location.
+     *  2. INTEGER t: integer, ending location of Sam's house location.
+     *  3. INTEGER a: integer, location of the Apple tree.
+     *  4. INTEGER b: integer, location of the Orange tree.
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+        long countApple = apples.stream()
+                .map(apple -> a + apple)
+                .filter(apple -> apple >= s && apple <= t)
+                .count();
+
+        long countOrange = oranges.stream()
+                .map(orange -> b + orange)
+                .filter(orange -> orange >= s && orange <= t)
+                .count();
+        System.out.println(countApple);
+        System.out.println(countOrange);
+    }
+}
---
+++ algorithms-and-data-structure/src/br/com/tony/hackerrank/GradingStudents.java
@@ -0,0 +1,44 @@
+package br.com.tony.hackerrank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true">Challenge Link</a>
+ * HackerLand University has the following grading policy:
+ * */
+public class GradingStudents {
+    public static void main(String[] args) {
+        List<Integer> grades = Arrays.asList(73, 67, 38, 33);
+        List<Integer> integers = gradingStudents(grades);
+        System.out.println(integers);
+    }
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        List<Integer> roundedGrades = new ArrayList<>();
+        for (Integer grade : grades) {
+            int diff = Math.abs(grade % 5 - 5);
+            if (grade < 38 || diff > 2) {
+                roundedGrades.add(grade);
+                continue;
+            }
+            roundedGrades.add(grade + diff);
+        }
+        return roundedGrades;
+    }
+}
+
+
+
+
+
+
+
+
