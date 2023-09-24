02cf88fb5c76a061283a02e032338ebc5e337686
tony <tony_luzsilva@hotmail.com>
2023-09-24 08:27:23 -0300

    feat: add challenge TimeConversion

---
+++ algorithms-and-data-structure/src/br/com/tony/hackerrank/TimeConversion.java
@@ -0,0 +1,23 @@
+package br.com.tony.hackerrank;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true">Challenge link</a>
+ * */
+public class TimeConversion {
+    public static void main(String[] args) {
+        System.out.println(timeConversion("07:05:45PM"));
+    }
+
+    /**
+     * Converter 12h time to 24h time, example:
+     * 07:05:45PM should converter to 19:05:45
+     * */
+    public static String timeConversion(String s) {
+        return LocalTime.parse(s, DateTimeFormatter.ofPattern("hh:mm:ssa", Locale.US))
+                .format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+}
